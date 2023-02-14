3f67a40e0c6f08d90f8c88cac5aff2df93a2db65
junesdream <norisori@gmail.com>
2023-02-14 12:43:14 +0100

    My third commit

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../App.css";
-import Signup from '../../assets/videos/sign-up.mp4';
+import Signup from '../../assets/videos/circle-rainbow.mp4';
 // export default function SignUp() {
 // 	return <h1 className="sign-up">LIKE & SUBSCRIBE</h1>;
 // }
